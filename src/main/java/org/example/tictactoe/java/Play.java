e2f9c21db9232dae7e040fd79401866bbe4d3d47
Piyush Purang <ppurang@gmail.com>
2011-09-08 15:02:42 +0200

    warn doesn't compile: game ending for others to work on

--- src/main/java/org/example/tictactoe/java/Play.java
+++ src/main/java/org/example/tictactoe/java/Play.java
@@ -16,6 +16,8 @@ public class Play {
             return this;
         }
 
+
+
         @Override
         public String toString() {
             return moves.toString();
@@ -89,6 +91,14 @@ public class Play {
                 .nextMove(new Move(3, new Cell(1, 2), Player.NOUGHT))
                 .nextMove(new Move(4, new Cell(2, 2), Player.CROSS))
                 .nextMove(new Move(5, new Cell(1, 3), Player.NOUGHT));
-        System.out.println(game);
+        if(game.isFinished()) {
+            if(game.hasWinner()) {
+                System.out.println("Winner is " + game.winner());
+            } else {
+                System.out.println("No winner!" );
+            }
+        } else {
+            System.out.println("Game isn't finished yet!" );
+        }
     }
 }
