6b681a065382e49025c68812a924958cec65e60e
Piyush Purang <ppurang@gmail.com>
2011-09-09 14:26:16 +0200

    changed indents from 4 to 2 spaces

--- src/main/java/org/example/tictactoe/java/Play.java
+++ src/main/java/org/example/tictactoe/java/Play.java
@@ -7,135 +7,135 @@ import java.util.Map;
  * @author Piyush Purang
  */
 public class Play {
-    public static final class Game {
-        private final Map<Cell,Move> moves = new HashMap<Cell,Move>();
+  public static final class Game {
+    private final Map<Cell, Move> moves = new HashMap<Cell, Move>();
 
-        public Game nextMove(final Move next) {
-            if(moves.get(next.getCell())==null){
-                moves.put(next.getCell(), next);
-            }else{
-                throw new IllegalStateException("move already exists " + next) ; // TODO provide a guard method
-            }
-            return this;
-        }
-
-        @Override
-        public String toString() {
-            final StringBuilder sb= new StringBuilder();
-            final String line="-------";
-            for (int i= 1; i < 4;i++){
-                sb.append(line).append("\n|");
-                for (int j= 1; j < 4;j++){
-                    final Cell cell = new Cell(i,j);
-                    if(moves.containsKey(cell)){
-                        sb.append(moves.get(cell).getPlayer()).append("|");
-                    }else{
-                        sb.append(" |");
-                    }
-                }
-                sb.append("\n");
-            }
-            sb.append(line);
-            return sb.toString();
-        }
-
-        public boolean isFinished() {
-            return true;
-        }
-
-        public boolean hasWinner() {
-            return true;
-        }
-
-        public Player winner() {
-            return Player.NOUGHT;
-        }
+    public Game nextMove(final Move next) {
+      if (moves.get(next.getCell()) == null) {
+        moves.put(next.getCell(), next);
+      } else {
+        throw new IllegalStateException("move already exists " + next); // TODO provide a guard method
+      }
+      return this;
     }
 
-    public static final class Move {
-        private final int number;
-        private final Cell cell;
-        private final Player player;
-
-        private Move(final int number, final Cell cell, final Player player) {
-            this.number = number;
-            this.cell = cell;
-            this.player = player;
+    @Override
+    public String toString() {
+      final StringBuilder sb = new StringBuilder();
+      final String line = "-------";
+      for (int i = 1; i < 4; i++) {
+        sb.append(line).append("\n|");
+        for (int j = 1; j < 4; j++) {
+          final Cell cell = new Cell(i, j);
+          if (moves.containsKey(cell)) {
+            sb.append(moves.get(cell).getPlayer()).append("|");
+          } else {
+            sb.append(" |");
+          }
         }
-
-        public Cell getCell() {
-            return cell;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%d %s %s", number, cell, player);
-        }
-
-        public Player getPlayer() {
-            return player;
-        }
+        sb.append("\n");
+      }
+      sb.append(line);
+      return sb.toString();
     }
 
-    // case class Cell(row: Int, cols: Int)
-    public static final class Cell {
-        private final int row;
-        private final int col;
-
-        private volatile int hashCode = 0;
-
-        public Cell(final int row, final int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("(%d, %d)", row, col);
-        }
-
-        @Override
-        public int hashCode() {
-            if(hashCode == 0) {
-                int result = 17;
-                result = 37 * result + (10 * row);
-                result = 37 * result + col;
-                hashCode = result;
-            }
-            return hashCode;
-        }
-
-        @Override
-        public boolean equals(final Object obj) {
-            if(this == obj) {
-                return true;
-            } else {
-                if(obj instanceof Cell) {
-                    final Cell that = (Cell) obj;
-                    return this.row == that.row && this.col == that.col;
-                } else {
-                    return false;
-                }
-            }
-        }
+    public boolean isFinished() {
+      return true;
     }
 
-    public static void main(final String[] args) {
-        final Game game = (new Game())
-                .nextMove(new Move(1, new Cell(1, 1), Player.NOUGHT))
-                .nextMove(new Move(2, new Cell(2, 1), Player.CROSS))
-                .nextMove(new Move(3, new Cell(1, 2), Player.NOUGHT))
-                .nextMove(new Move(4, new Cell(2, 2), Player.CROSS))
-                .nextMove(new Move(5, new Cell(1, 3), Player.NOUGHT));
-        System.out.println(game);
-        if(game.isFinished()) {
-            if(game.hasWinner()) {
-                System.out.println("Winner is " + game.winner());
-            } else {
-                System.out.println("No winner!" );
-            }
+    public boolean hasWinner() {
+      return true;
+    }
+
+    public Player winner() {
+      return Player.NOUGHT;
+    }
+  }
+
+  public static final class Move {
+    private final int number;
+    private final Cell cell;
+    private final Player player;
+
+    private Move(final int number, final Cell cell, final Player player) {
+      this.number = number;
+      this.cell = cell;
+      this.player = player;
+    }
+
+    public Cell getCell() {
+      return cell;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("%d %s %s", number, cell, player);
+    }
+
+    public Player getPlayer() {
+      return player;
+    }
+  }
+
+  // case class Cell(row: Int, cols: Int)
+  public static final class Cell {
+    private final int row;
+    private final int col;
+
+    private volatile int hashCode = 0;
+
+    public Cell(final int row, final int col) {
+      this.row = row;
+      this.col = col;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("(%d, %d)", row, col);
+    }
+
+    @Override
+    public int hashCode() {
+      if (hashCode == 0) {
+        int result = 17;
+        result = 37 * result + (10 * row);
+        result = 37 * result + col;
+        hashCode = result;
+      }
+      return hashCode;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+      if (this == obj) {
+        return true;
+      } else {
+        if (obj instanceof Cell) {
+          final Cell that = (Cell) obj;
+          return this.row == that.row && this.col == that.col;
         } else {
-            System.out.println("Game isn't finished yet!" );
+          return false;
         }
+      }
     }
+  }
+
+  public static void main(final String[] args) {
+    final Game game = (new Game())
+                        .nextMove(new Move(1, new Cell(1, 1), Player.NOUGHT))
+                        .nextMove(new Move(2, new Cell(2, 1), Player.CROSS))
+                        .nextMove(new Move(3, new Cell(1, 2), Player.NOUGHT))
+                        .nextMove(new Move(4, new Cell(2, 2), Player.CROSS))
+                        .nextMove(new Move(5, new Cell(1, 3), Player.NOUGHT));
+    System.out.println(game);
+    if (game.isFinished()) {
+      if (game.hasWinner()) {
+        System.out.println("Winner is " + game.winner());
+      } else {
+        System.out.println("No winner!");
+      }
+    } else {
+      System.out.println("Game isn't finished yet!");
+    }
+  }
 }
--- src/main/java/org/example/tictactoe/java/Player.java
+++ src/main/java/org/example/tictactoe/java/Player.java
@@ -1,28 +1,30 @@
 package org.example.tictactoe.java;
 
-public enum Player  {
-    NOUGHT() {
-        @Override
-        public Player nextPlayer() {
-           return CROSS;
-       }
-        @Override
-        public String toString() {
-            return "O";
-        }
-    },
-    CROSS() {
-        @Override
-        public Player nextPlayer() {
-           return NOUGHT;
-       }
-        @Override
-        public String toString() {
-            return "X";
-        }
-    };
-
+public enum Player {
+  NOUGHT() {
+    @Override
     public Player nextPlayer() {
-        throw new AbstractMethodError();
+      return CROSS;
     }
+
+    @Override
+    public String toString() {
+      return "O";
+    }
+  },
+  CROSS() {
+    @Override
+    public Player nextPlayer() {
+      return NOUGHT;
+    }
+
+    @Override
+    public String toString() {
+      return "X";
+    }
+  };
+
+  public Player nextPlayer() {
+    throw new AbstractMethodError();
+  }
 }
